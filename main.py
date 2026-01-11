29b3d93beed1d8794da586505db0202825540839
brul1ka <yr.bunik@gmail.com>
2026-01-11 10:53:14 +0200

    code formatted, clicker can load your robots from save file, robot logic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import customtkinter as ctk
 from shop import ShopWindow
+from entities import Robot
+
 
 class ClickerApp(ctk.CTk):
     def __init__(self):
@@ -10,6 +12,7 @@ class ClickerApp(ctk.CTk):
         self.points = 0
         self.points_per_click = 1
         self.price_for_upgrade = 10
+        self.robots = [Robot("Beginner", 1, 200, False)]
 
         self.geometry("600x450")
         self.title("Classic Clicker")
@@ -17,14 +20,33 @@ class ClickerApp(ctk.CTk):
 
         self.load_save()
 
-        self.your_points_label = ctk.CTkLabel(self, text=f"Your points: {self.points}",font=("Arial", 24))
+        self.your_points_label = ctk.CTkLabel(
+            self, text=f"Your points: {self.points}", font=("Arial", 24)
+        )
         self.your_points_label.grid(row=0, column=0, padx=20, pady=(20, 60), sticky="w")
-        self.main_button = ctk.CTkButton(self, text="", width=200, height=200, corner_radius=100, border_width=3, hover_color="#3B4482", command=self.update_points)
-        self.main_button.grid(row=1, column=0, pady=(0,50))
-        self.upgrade_button = ctk.CTkButton(self, text=f"Upgrade button (Cost: {self.price_for_upgrade})", command=self.upgrade_main_button)
+        self.main_button = ctk.CTkButton(
+            self,
+            text="",
+            width=200,
+            height=200,
+            corner_radius=100,
+            border_width=3,
+            hover_color="#3B4482",
+            command=self.update_points,
+        )
+        self.main_button.grid(row=1, column=0, pady=(0, 50))
+        self.upgrade_button = ctk.CTkButton(
+            self,
+            text=f"Upgrade button (Cost: {self.price_for_upgrade})",
+            command=self.upgrade_main_button,
+        )
         self.upgrade_button.grid(row=2, column=0)
-        self.shop_button = ctk.CTkButton(self, text="To the shop", command=self.open_shop)
+        self.shop_button = ctk.CTkButton(
+            self, text="To the shop", command=self.open_shop
+        )
         self.shop_button.grid(row=1, column=1)
+
+        self.auto_click()
 
     def update_points(self):
         self.points = self.points + self.points_per_click
@@ -37,7 +59,9 @@ class ClickerApp(ctk.CTk):
             self.price_for_upgrade *= 10
 
             self.your_points_label.configure(text=f"Your points: {self.points}")
-            self.upgrade_button.configure(text=f"Upgrade button (Cost: {self.price_for_upgrade})")
+            self.upgrade_button.configure(
+                text=f"Upgrade button (Cost: {self.price_for_upgrade})"
+            )
 
             self.save_game()
 
@@ -52,6 +76,8 @@ class ClickerApp(ctk.CTk):
             file.write(str(self.points) + "\n")
             file.write(str(self.points_per_click) + "\n")
             file.write(str(self.price_for_upgrade) + "\n")
+            for robot in self.robots:
+                file.write(str(1) + "\n") if robot.exists else file.write(str(0) + "\n")
 
     def on_closing(self):
         self.save_game()
@@ -61,18 +87,34 @@ class ClickerApp(ctk.CTk):
         try:
             with open("save.txt", "r") as file:
                 lines = file.readlines()
-                if len(lines) >= 3:
-                    try:
+                try:
+                    if len(lines) >= 3:
                         self.points = int(lines[0])
                         self.points_per_click = int(lines[1])
                         self.price_for_upgrade = int(lines[2])
-                    except ValueError:
-                        print("ERROR: something isn't integer in save.txt, restoring to default values")
-                        self.points = 0
-                        self.points_per_click = 1
-                        self.price_for_upgrade = 10
+
+                        if len(lines) >= 4:
+                            for i, robot in enumerate(self.robots):
+                                robot.exists = bool(int(lines[3 + i]))
+                except ValueError:
+                    print(
+                        "ERROR: something isn't integer in save.txt, restoring to default values"
+                    )
+                    self.points = 0
+                    self.points_per_click = 1
+                    self.price_for_upgrade = 10
         except FileNotFoundError:
-            print("ERROR: no save file")
+            print("INFO: no save file")
+
+    def auto_click(self):
+        total_income = 0
+        for i, robot in enumerate(self.robots):
+            if robot.exists:
+                total_income += robot.power
+
+        self.points += total_income
+        self.your_points_label.configure(text=f"Your points: {self.points}")
+        self.after(1000, self.auto_click)
 
 
 if __name__ == "__main__":
