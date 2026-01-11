f4bfdb2cd2e4c8d07835506a01282bd5a5ed77c5
brul1ka <yr.bunik@gmail.com>
2026-01-11 10:56:25 +0200

    implement robot buying and real-time button coloring

--- shop.py
+++ shop.py
@@ -1,10 +1,40 @@
 import customtkinter as ctk
 
+
 class ShopWindow(ctk.CTkToplevel):
     def __init__(self, master):
         super().__init__(master)
-        self.geometry("300x200")
-        self.title("Shop")
 
-        header = ctk.CTkLabel(self, text="Welcome to the shop!")
-        header.grid(row=0, column=0)
+        self.button_list = []
+
+        self.geometry("500x450")
+        self.title("Robot shop")
+
+        for i, robot in enumerate(self.master.robots):
+            button = ctk.CTkButton(
+                self,
+                text=f"Buy {robot.name}, price: {robot.price}, gives {robot.power} points in sec",
+            )
+            button.configure(command=lambda r=robot, b=button: self.buy_robot(r, b))
+            button.pack(fill="x", pady=15, padx=20)
+            self.button_list.append((button, robot))
+
+        self.update_buttons()
+
+    def buy_robot(self, robot, button):
+        if self.master.points >= robot.price:
+            self.master.points -= robot.price
+            robot.exists = True
+            self.master.your_points_label.configure(
+                text=f"Your points: {self.master.points}"
+            )
+
+    def update_buttons(self):
+        for btn, robot in self.button_list:
+            btn.configure(
+                hover_color=(
+                    "dark green" if self.master.points >= robot.price else "dark red"
+                ),
+                fg_color="green" if self.master.points >= robot.price else "red",
+            )
+        self.after(100, self.update_buttons)
