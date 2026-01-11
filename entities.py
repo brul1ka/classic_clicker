98203f1ba4a88869596ed53293d379ec79c568ab
brul1ka <yr.bunik@gmail.com>
2026-01-11 12:25:14 +0200

    new param

--- entities.py
+++ entities.py
@@ -1,6 +1,7 @@
 class Robot:
-    def __init__(self, name, power, price, exists):
+    def __init__(self, name, power, price, exists, how_many):
         self.name = name
         self.power = power
         self.price = price
         self.exists = exists
+        self.how_many = how_many
